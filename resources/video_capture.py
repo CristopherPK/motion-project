5f945a83e7315fc17749f3c227b8e4d61a681f56
Cristopher Freitas <cristopherpk@gmail.com>
2014-09-17 16:52:22 -0300

    Working at model.

--- resources/video_capture.py
+++ resources/video_capture.py
@@ -2,30 +2,29 @@
 Created on Sep 11, 2014
 
 @author: cristopher
+@contact: cristopherpk@gmail.com
 '''
 
-import cv2
+import cv2 as cv
 
 class Video(object):
     
-    def play(self,videoName,videoPath):
-
-        cap = cv2.VideoCapture()
-        cap.open(videoPath)    
-        print cap.isOpened()        
-        cv2.namedWindow(videoName, cv2.WINDOW_AUTOSIZE)
-        #frames = cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
-        #print frames                        
-                              
-        while cap.isOpened() is True:
-            frame = cap.read()
-            #print frame
-            if frame[0] is False:
-                break
-            
-            cv2.imshow(videoName, frame[1])
-            
-        cv2.waitKey(0)            
+    def capture(self, video):
+        videoCapture = cv.VideoCapture(video)
+        return videoCapture
+    
+    def getFps(self, videoCapture):
+        fps = videoCapture.get(cv.cv.CV_CAP_PROP_FPS)
+        return fps
+    
+    def getSize(self, videoCapture):
+        size = (int(videoCapture.get(cv.cv.CV_CAP_PROP_FRAME_WIDTH)),
+                int(videoCapture.get(cv.cv.CV_CAP_PROP_FRAME_HEIGHT)))
+        return size
+    
+    def write(self, video, fps, size):
+        videoWriter = cv.VideoWriter(video, cv.cv.CV_FOURCC('I','4','2','0'), fps, size)
+        return videoWriter
 
 if __name__ == '__main__':
                 
